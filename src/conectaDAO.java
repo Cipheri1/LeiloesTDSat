4ddfa9e480689154622671546a5002d1c8d8c3b5
Matheus Paim <calheiros527@gmail.com>
2026-03-03 21:04:29 -0300

    feat: banco de dados criado e conexão estabelecida no projeto

--- src/conectaDAO.java
+++ src/conectaDAO.java
@@ -22,7 +22,10 @@ public class conectaDAO {
         
         try {
         
-            conn = DriverManager.getConnection("jdbc:mysql://localhost/uc11?user=root&password=");
+            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/bd_uc11",
+                    "root",
+                    "1234"
+                );
             
         } catch (SQLException erro){
             JOptionPane.showMessageDialog(null, "Erro ConectaDAO" + erro.getMessage());
